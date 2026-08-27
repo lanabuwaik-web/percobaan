faa07483ee39294dca8ebbcc1a7772fd0b4c40fb
ajiramandani111-ux <ajiramandani111@gmail.com>
2026-08-28 06:09:05 +0700

    Update coding

--- Belajar/Belajar1/basic.java
+++ Belajar/Belajar1/basic.java
@@ -7,17 +7,26 @@ public class basic {
         System.out.print("Masukkan nama: ");
         String nama = input.nextLine();
 
+        System.out.print("Masukkan NIM: ");
+        String nim = input.nextLine();
+
         System.out.print("Masukkan umur: ");
         int umur = input.nextInt();
+        input.nextLine(); // membersihkan Enter
+
+        System.out.print("Masukkan alamat: ");
+        String alamat = input.nextLine();
 
         System.out.println("\n=== Data Diri ===");
-        System.out.println("Nama : " + nama);
-        System.out.println("Umur : " + umur + " tahun");
+        System.out.println("Nama   : " + nama);
+        System.out.println("NIM    : " + nim);
+        System.out.println("Umur   : " + umur + " tahun");
+        System.out.println("Alamat : " + alamat);
 
         if (umur >= 17) {
-            System.out.println("Status: Sudah cukup umur");
+            System.out.println("Status : Sudah cukup umur");
         } else {
-            System.out.println("Status: Belum cukup umur");
+            System.out.println("Status : Belum cukup umur");
         }
 
         input.close();
